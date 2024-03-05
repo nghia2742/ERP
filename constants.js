44e9311b97e3c33891177ef53ab59813233d2c3d
Trong Nghia <nghia.nt2704@gmail.com>
2024-03-05 12:56:01 +0700

    Connected to api live server

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 // export const SERVER_BASE_URL = "http://127.0.0.1:8000"
-export const SERVER_BASE_URL = "https://api-erp.onrender.com"
+export const SERVER_BASE_URL = "https://api-erp-a6hc.onrender.com"
